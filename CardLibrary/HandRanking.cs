61ca897113087bbbfdb6a8cf14e07cff349319fd
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-03-25 21:05:49 -0400

    Added hand rankings enum class

---
+++ CardLibrary/HandRanking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardLibrary
+{
+    public enum HandRanking
+    {
+        HighCard = 1,
+        OnePair,
+        TwoPair,
+        ThreeOfAKind,
+        Straight,
+        Flush,
+        FullHouse,
+        FourOfAKind,
+        StraightFlush,
+        RoyalFlush,
+    }
+}
